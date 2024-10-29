d323bfb43a0cb4d6c2d76995a6f1fa34065583d2
Daniil <krupa.daniil94@outlook.com>
2024-10-29 15:52:37 +0200

    adding validations

--- src/main/resources/static/js/generalScripts.js
+++ src/main/resources/static/js/generalScripts.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-    let flInput = document.getElementById("userPhone");
-    let ps_1 = document.getElementById("userPassword");
-    let ps_2 = document.getElementById("confirmPassword");
+    let p_Name = document.getElementById("userName");
+    let p_Surname = document.getElementById("userSurname");
+    let p_Phone = document.getElementById("userPhone");
+    let p_Email = document.getElementById("userEmail");
+    let p_Password = document.getElementById("userPassword");
 
 
-    flInput.addEventListener("keypress", (e) => {
+    p_Phone.addEventListener("keypress", (e) => {
         if (!/\d/.test(e.key)) {
             e.preventDefault();
         }
     })
+
+    let regex_1 = new RegExp(/^[a-zA-Zа-яА-ЯІіЇї']*$/);
+    let regex_2 = new RegExp(/^\w+@\w+\.[a-zA-Z]+(\.[a-zA-Z]+)?$/);
+
+    p_Name.addEventListener("input", (e) => {
+        testRegex(regex_1, p_Name)
+    });
+
+    function testRegex(regex, param) {
+        if (!regex.test(param.value)) {
+            param.classList.add("invalidData");
+        }
+        else {
+            param.classList.remove("invalidData");
+        }
+    }
 });
